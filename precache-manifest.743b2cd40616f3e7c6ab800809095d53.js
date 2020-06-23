64aeecb21f58132c6ca87e9c66b5a61dc337e9bb
Aalok Agrawal <aalokagrwal@iitkgp.ac.in>
2020-06-23 18:44:21 +0530

    Updates

---
+++ precache-manifest.743b2cd40616f3e7c6ab800809095d53.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48af7da840802ddecc74b7eb74bc154",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "3412c0d88ebae6c8f1bd",
+    "url": "/app/static/css/main.c1e22791.chunk.css"
+  },
+  {
+    "revision": "2a0ebac2dc2b415d4a21",
+    "url": "/app/static/js/2.81688b44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app/static/js/2.81688b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3412c0d88ebae6c8f1bd",
+    "url": "/app/static/js/main.9132d307.chunk.js"
+  },
+  {
+    "revision": "baa489da34425f16a173",
+    "url": "/app/static/js/runtime-main.12ceb904.js"
+  },
+  {
+    "revision": "c268558484c0bb6b2c594b0dbf3f8b72",
+    "url": "/app/static/media/bgimage.c2685584.jpg"
+  },
+  {
+    "revision": "957d66d0bbd7e2869c8d0d3783f98ad4",
+    "url": "/app/static/media/chemistry.957d66d0.jpg"
+  },
+  {
+    "revision": "cad0b22ba74083ecb2929124f38d2e0c",
+    "url": "/app/static/media/cs.cad0b22b.jpg"
+  },
+  {
+    "revision": "bec595c2271c3a94fad90c2505ec9a83",
+    "url": "/app/static/media/ee.bec595c2.jpg"
+  },
+  {
+    "revision": "c3b6db1df699effa1d2e0779b1e427a8",
+    "url": "/app/static/media/eng.c3b6db1d.jpg"
+  },
+  {
+    "revision": "1406e94bcab7b65cb4fbf4ecf49bcb7c",
+    "url": "/app/static/media/maths.1406e94b.jpg"
+  },
+  {
+    "revision": "29efd3bb02f8676067f5440f700cc663",
+    "url": "/app/static/media/phy.29efd3bb.jpg"
+  }
+]);
